a8c9949f63364a0e627f757ac974e3bf0d7ea156
hongh931 <hn582183@dal.ca>
2025-10-29 16:17:15 -0300

    clean up all

--- backend/api/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-FastAPI routers for the NASA ice backend.
-"""
-
-__all__ = ["ice_extent", "route_prediction"]
--- frontend/src/api/mapAPI.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import parsedEnv from "../config/env";
-
-const api = axios.create({
-  baseURL: `${parsedEnv.VITE_API_BASE}${parsedEnv.VITE_API_PREFIX}`,
-  withCredentials: true
-});
-
-export default api;
